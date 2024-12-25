db27f4c7558bfbac0954a744417ba54fddd3d391
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-12-24 16:43:26 -0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,15 @@ fn main() {
         match parts.next().unwrap() {
             "exit" => std::process::exit(parts.next().and_then(|x| x.parse().ok()).unwrap_or(0)),
             "echo" => println!("{}", parts.collect::<Vec<&str>>().join(" ")),
+            "type" => {
+                let command = parts.next().unwrap_or("");
+                match command {
+                    "echo" => println!("echo is a shell builtin"),
+                    "type" => println!("type is a shell builtin"),
+                    "exit" => println!("exit is a shell builtin"),
+                    _ => println!("{}: not found", command),
+                }
+            },
             _ => println!("{input}: command not found"),
         }
     }
